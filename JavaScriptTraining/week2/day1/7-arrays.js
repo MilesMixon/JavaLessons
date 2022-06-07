a29ccf9b3d8736efe16f26ba6d1b46f285e84a72
seancarter99 <scarter@skillstorm.com>
2022-06-07 15:13:00 -0400

    Adding afternoon examples

---
+++ JavaScriptTraining/week2/day1/7-arrays.js
@@ -0,0 +1,13 @@
+// Object[] myArray = {"", 1, false};
+
+// const arr = new Array(); // Another way to create an array
+
+const myArray = ["", 1, false];
+
+const fruits = ['Mango', 'Banana', 'Peach', 'Pear'];
+
+console.log('Fruits before pop:' + fruits);
+
+fruits.pop();
+
+console.log('Fruits after pop:' + fruits);
